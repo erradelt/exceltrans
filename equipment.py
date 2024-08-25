238a2a72b4456eb1e6a4d1b20ab4c8e15a121bd4
radelt <137507960+erradelt@users.noreply.github.com>
2024-08-25 09:08:37 +0200

    Add files via upload

---
+++ equipment.py
@@ -0,0 +1,15 @@
+equipment_types: dict = {
+        'RG_' : 'Regale',
+        'SHS' : 'Sicherheitsschränke',
+        'HL_' : 'Hakenleisten',
+        'TP_' : 'Tischplatten',
+        'HS_' : 'Hochschränke',
+        'HA_' : 'Hospitalausguss',
+        'US_' : 'Unterbauschränke',
+        'WS_' : 'Wandhängeschränke',
+        'ST_' : 'Schreibtische',
+        'RC_' : 'Rollcontainer',
+        'MA_' : 'Medienampeln',
+        'nan' : 'Sonstiges',
+        'XXX' : 'unbekannt'
+        }
