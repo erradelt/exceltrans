00f3ca146c29eaf7807550fc5e81cebd9cd8c2dc
radelt <137507960+erradelt@users.noreply.github.com>
2024-10-26 19:31:23 +0200

    Add files via upload

--- gui.py
+++ gui.py
@@ -4,25 +4,12 @@ import os
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QLabel, QWidget, QHBoxLayout, 
                             QVBoxLayout, QFormLayout, QLineEdit, QPushButton, QFileDialog,
-                            QMessageBox, QProgressBar, QCheckBox)
+                            QMessageBox, QCheckBox)
 
 import pandas as pd
 
 import maincode
 import filepathgen as fg
-
-# Custom QThread class to handle the Converter in a separate thread
-class ConverterThread(QtCore.QThread):
-    progress = QtCore.pyqtSignal(int)  # Define a signal to emit progress
-
-    def __init__(self, file_path):
-        super().__init__()
-        self.file_path = file_path
-
-    def run(self):
-        converter = maincode.Converter(self.file_path, progress_callback=self.progress.emit)
-        self.progress.emit(100)  # Ensure progress bar hits 100% when done
-
 
 class Ui_MainWindow(QMainWindow):
     def __init__(self):
@@ -43,12 +30,12 @@ class Interface(QWidget):
         self.full_path = ''
 
     def controls(self):
-        # efine layouts and widgets
+        # define layouts and widgets
         self.layout = QVBoxLayout(self) #masterlayout
         self.layout_top = QHBoxLayout(self)
         self.layout_path = QHBoxLayout(self)
         self.layout_name = QHBoxLayout(self)
-        self.layout_check = QHBoxLayout(self)
+        self.layout_check = QVBoxLayout(self)
         self.find_excel = QPushButton('feed me!')
         self.conv_excel = QPushButton('konvertieren')
         self.conv_excel.setEnabled(False)
@@ -56,9 +43,11 @@ class Interface(QWidget):
         self.path_label_txt = QLabel()
         self.excel_name_label = QLabel('Dateiname')
         self.excel_name = QLabel()
-        self.progress_bar = QProgressBar()
-        self.progress_bar.setValue(0)
         self.neworold = QCheckBox('Neuen Dateinamen nach Konvertierung verwenden')
+        self.neworold.setEnabled(False)
+        self.neworold.setChecked(False) #newname disabled by default
+        self.newname = QLineEdit()
+        self.newname.setEnabled(False)
         # add widgets to layout
         self.layout_top.addWidget(self.find_excel)
         self.layout_top.addWidget(self.conv_excel)
@@ -67,19 +56,19 @@ class Interface(QWidget):
         self.layout_name.addWidget(self.excel_name_label)
         self.layout_name.addWidget(self.excel_name)
         self.layout_check.addWidget(self.neworold)
+        self.layout_check.addWidget(self.newname)
         # add layouts to masterlayout
         self.layout.addLayout(self.layout_top)
         self.layout.addLayout(self.layout_path)
         self.layout.addLayout(self.layout_name)
         self.layout.addLayout(self.layout_check)
-        self.layout.addWidget(self.progress_bar)
         # connect buttons to methods
         self.find_excel.clicked.connect(self.excelgrabber)
         self.conv_excel.clicked.connect(self.excelconverter)
+        self.neworold.stateChanged.connect(self.inputenabler)
     
 
     def excelgrabber(self):
-        self.conv_excel.setEnabled(False)
         self.path_label_txt.setText('')
         self.excel_name.setText('')
         self.expath = QFileDialog.getOpenFileName(None,caption= 'open file', directory=fg.current_directory)
@@ -91,19 +80,28 @@ class Interface(QWidget):
                 self.path_label_txt.setText(self.dirpath)
                 self.excel_name.setText(self.filename)
                 self.conv_excel.setEnabled(True)
+                self.neworold.setEnabled(True)
             except ValueError:
                 self.msg = QMessageBox(icon=Warning, text='keine Excel-Datei ausgewählt')
                 self.msg.show()
 
     def excelconverter(self):
-        maincode.Converter(self.full_path)
+        if self.neworold.isChecked():
+            if not self.newname.text():
+                print('keine name')
+            else:
+                self.temptext = self.newname.text()
+                maincode.Converter(self.full_path, self.temptext)   
+        else:
+            maincode.Converter(self.full_path, self.excel_name.text().rstrip('.xlsx'))
 
-        self.thread = ConverterThread(self.full_path)
-        self.thread.progress.connect(self.update_progress_bar)
-        self.thread.start()
+        
 
-    def update_progress_bar(self, value):
-        self.progress_bar.setValue(value)
+    def inputenabler(self, state):
+        if state == 2:
+            self.newname.setEnabled(True)
+        else:
+            self.newname.setEnabled(False)
 
 def main():
     app = QApplication(sys.argv)
